bd7d3fa046d8dde2ac8d69a33d72925c0a0d9bd3
Handi Wijoyo <handiwijoyo@gmail.com>
2016-12-09 16:39:05 +0700

    Add ProductCategory model & migration

---
+++ database/migrations/2016_12_09_093545_create_product_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('display_name');
+            $table->integer('parent_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
