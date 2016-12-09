eb525c9dbc51a81aca9838c6c0901fc8b4722a60
Handi Wijoyo <handiwijoyo@gmail.com>
2016-12-09 15:30:15 +0700

    Add Product model and migration

---
+++ database/migrations/2016_12_09_082651_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sku')->unique()->index();
+            $table->string('name')->index();
+            $table->string('description')->nullable();
+            $table->string('composition')->nullable();
+            $table->string('care_label')->nullable();
+            $table->string('measurement')->nullable();
+            $table->boolean('active')->default(0);
+            $table->text('images')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
