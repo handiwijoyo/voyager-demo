650fc0d9abec21014ebfd5ed3b151e9fe301c717
Handi Wijoyo <handiwijoyo@gmail.com>
2016-12-09 17:01:22 +0700

    Add product categories view

---
+++ resources/views/vendor/voyager/product-categories/edit-add.blade.php
@@ -0,0 +1,104 @@
+@extends('voyager::master')
+
+@section('css')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@stop
+
+@section('page_header')
+    <h1 class="page-title">
+        <i class="{{ $dataType->icon }}"></i> @if(isset($dataTypeContent->id)){{ 'Edit' }}@else{{ 'New' }}@endif {{ $dataType->display_name_singular }}
+    </h1>
+@stop
+
+@section('content')
+    <div class="page-content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="panel panel-bordered">
+
+                    <div class="panel-heading">
+                        <h3 class="panel-title">@if(isset($dataTypeContent->id)){{ 'Edit' }}@else{{ 'Add New' }}@endif {{ $dataType->display_name_singular }}</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                    <form role="form"
+                          action="@if(isset($dataTypeContent->id)){{ route('voyager.'.$dataType->slug.'.update', $dataTypeContent->id) }}@else{{ route('voyager.'.$dataType->slug.'.store') }}@endif"
+                          method="POST" enctype="multipart/form-data">
+                        <!-- PUT Method if we are editing -->
+                    @if(isset($dataTypeContent->id))
+                        {{ method_field("PUT") }}
+                    @endif
+
+                    <!-- CSRF TOKEN -->
+                        {{ csrf_field() }}
+
+                        <div class="panel-body">
+
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+
+                            <div class="form-group">
+                                <label for="name">Name</label> <small>(use for category URL)</small>
+                                <input type="text" class="form-control" name="name" id="name"
+                                       placeholder="name"
+                                       value="@if(isset($dataTypeContent->name)){{ old('name', $dataTypeContent->name) }}@else{{old('name')}}@endif">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="display_name">Display Name</label>
+                                <input type="text" class="form-control" name="display_name" id="display_name"
+                                       placeholder="Display Name"
+                                       value="@if(isset($dataTypeContent->display_name)){{ old('display_name', $dataTypeContent->display_name) }}@else{{old('display_name')}}@endif">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="parent_id">Parent Category</label>
+                                <?php $selected = (isset($dataTypeContent->parent_id)) ? old('parent_id', $dataTypeContent->parent_id) : old('parent_id'); ?>
+                                <select name="parent_id" id="parent_id" class="form-control">
+                                    <option value="0">-- Select Parent Category --</option>
+                                    @foreach(\App\ProductCategory::where('parent_id', 0)->get() as $item)
+                                        <option value="{{$item->id}}">{{$item->display_name}}</option>
+                                    @endforeach
+                                </select>
+
+                            </div>
+
+                        </div><!-- panel-body -->
+
+                        <div class="panel-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+
+                    <iframe id="form_target" name="form_target" style="display:none"></iframe>
+                    <form id="my_form" action="{{ route('voyager.upload') }}" target="form_target" method="post"
+                          enctype="multipart/form-data" style="width:0;height:0;overflow:hidden">
+                        <input name="image" id="upload_file" type="file"
+                               onchange="$('#my_form').submit();this.value='';">
+                        <input type="hidden" name="type_slug" id="type_slug" value="{{ $dataType->slug }}">
+                        {{ csrf_field() }}
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('javascript')
+    <script>
+        $('document').ready(function () {
+            $('.toggleswitch').bootstrapToggle();
+        });
+    </script>
+    <script src="{{ config('voyager.assets_path') }}/lib/js/tinymce/tinymce.min.js"></script>
+    <script src="{{ config('voyager.assets_path') }}/js/voyager_tinymce.js"></script>
+@stop
