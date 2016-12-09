f9c4ab519fee1ee8363291d84c891db48a482896
Handi Wijoyo <handiwijoyo@gmail.com>
2016-12-09 17:18:41 +0700

    Update product categories seeder

--- database/seeds/ProductCategoriesDataTypeSeeder.php
+++ database/seeds/ProductCategoriesDataTypeSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use TCG\Voyager\Models\DataRow;
 use TCG\Voyager\Models\DataType;
 use TCG\Voyager\Models\Menu;
 use TCG\Voyager\Models\Permission;
@@ -34,9 +35,9 @@ class ProductCategoriesDataTypeSeeder extends Seeder
         $menu = Menu::where('name', 'admin')->firstOrFail();
 
         $menuItem = MenuItem::firstOrNew([
-            'menu_id'    => $menu->id,
-            'title'      => 'Product Categories',
-            'url'        => route('voyager.product-categories.index', [], false),
+            'menu_id' => $menu->id,
+            'title'   => 'Product Categories',
+            'url'     => route('voyager.product-categories.index', [], false),
         ]);
         if (!$menuItem->exists) {
             $menuItem->fill([
@@ -45,6 +46,122 @@ class ProductCategoriesDataTypeSeeder extends Seeder
                 'color'      => null,
                 'parent_id'  => null,
                 'order'      => 12,
+            ])->save();
+        }
+
+        $dataRow = DataRow::firstOrNew([
+            'data_type_id' => $dataType->id,
+            'field'        => 'id',
+        ]);
+        if (!$dataRow->exists) {
+            $dataRow->fill([
+                'type'         => 'PRI',
+                'display_name' => 'Id',
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 0,
+                'edit'         => 0,
+                'add'          => 0,
+                'delete'       => 0,
+                'details'      => '',
+            ])->save();
+        }
+
+        $dataRow = DataRow::firstOrNew([
+            'data_type_id' => $dataType->id,
+            'field'        => 'name',
+        ]);
+        if (!$dataRow->exists) {
+            $dataRow->fill([
+                'type'         => 'text',
+                'display_name' => 'Name',
+                'required'     => 1,
+                'browse'       => 1,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'details'      => '{
+"validation": {
+"rule": "required"
+}
+}',
+            ])->save();
+        }
+
+        $dataRow = DataRow::firstOrNew([
+            'data_type_id' => $dataType->id,
+            'field'        => 'display_name',
+        ]);
+        if (!$dataRow->exists) {
+            $dataRow->fill([
+                'type'         => 'text',
+                'display_name' => 'Display Name',
+                'required'     => 1,
+                'browse'       => 1,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'details'      => '{
+"validation": {
+"rule": "required"
+}
+}',
+            ])->save();
+        }
+
+        $dataRow = DataRow::firstOrNew([
+            'data_type_id' => $dataType->id,
+            'field'        => 'parent_id',
+        ]);
+        if (!$dataRow->exists) {
+            $dataRow->fill([
+                'type'         => 'text',
+                'display_name' => 'Parent Id',
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 0,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 0,
+                'details'      => '',
+            ])->save();
+        }
+
+        $dataRow = DataRow::firstOrNew([
+            'data_type_id' => $dataType->id,
+            'field'        => 'created_at',
+        ]);
+        if (!$dataRow->exists) {
+            $dataRow->fill([
+                'type'         => 'timestamp',
+                'display_name' => 'Created At',
+                'required'     => 0,
+                'browse'       => 0,
+                'read'         => 0,
+                'edit'         => 0,
+                'add'          => 0,
+                'delete'       => 0,
+                'details'      => '',
+            ])->save();
+        }
+
+        $dataRow = DataRow::firstOrNew([
+            'data_type_id' => $dataType->id,
+            'field'        => 'updated_at',
+        ]);
+        if (!$dataRow->exists) {
+            $dataRow->fill([
+                'type'         => 'timestamp',
+                'display_name' => 'Updated At',
+                'required'     => 0,
+                'browse'       => 0,
+                'read'         => 0,
+                'edit'         => 0,
+                'add'          => 0,
+                'delete'       => 0,
+                'details'      => '',
             ])->save();
         }
     }
